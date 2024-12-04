96b12c73bb7816a4a552489f1c41ec48c748b9fb
EugeneSamsonov <samsonovevgenii231@gmail.com>
2024-12-04 17:58:41 +0300

    add visibility on other devices

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f%3ilzy(dj9x-&+rcmo&x)3ofvwbqf56y2!=6duvygcf1_sx(q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
